c12ce0b5953f71edd0fe539371aa5c1a7765d0c6
Helen Abraham <AbrahamH1@cardiff.ac.uk>
2019-11-14 12:21:57 +0000

    Initial Commit

---
+++ src/main/java/com/ons/group2/ons_client_project/OnsClientProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ons.group2.ons_client_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnsClientProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnsClientProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ons/group2/ons_client_project/OnsClientProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ons.group2.ons_client_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnsClientProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
