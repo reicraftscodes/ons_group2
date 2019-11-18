addab04ba393d5936237c7e356f0f9c4555c351b
Soma Somorjai <somorjais@cardiff.ac.uk>
2019-11-18 19:55:38 +0000

    Adding password change

--- src/main/java/com/ons/group2/ons_client_project/controllers/AccountController.java
+++ src/main/java/com/ons/group2/ons_client_project/controllers/AccountController.java
@@ -3,15 +3,18 @@ package com.ons.group2.ons_client_project.controllers;
 import com.ons.group2.ons_client_project.model.User;
 import com.ons.group2.ons_client_project.model.dto.account.ChangePasswordDto;
 import com.ons.group2.ons_client_project.service.UserService;
-import org.springframework.http.ResponseEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/Account")
+@SessionAttributes({"user"})
+@Slf4j
 public class AccountController {
 
     private final UserService userService;
@@ -21,10 +24,29 @@ public class AccountController {
     }
 
     @PostMapping("/ChangePassword")
-    public String changePassword(@Valid ChangePasswordDto changePasswordDto) {
+    public String changePassword(@ModelAttribute("changePasswordDto") @Valid ChangePasswordDto changePasswordDto,
+                                 @SessionAttribute("user") User user,
+                                 BindingResult bindingResult) {
 
-        userService.changePassword(changePasswordDto.user, changePasswordDto.newPassword);
+        log.info("Change Password Dto: " + changePasswordDto);
+        log.info("Change Password For user: " + user);
 
-        return "redirect:/profiles/" + changePasswordDto.user.getId();
+        if(!changePasswordDto.newPassword.equals(changePasswordDto.newPasswordCf)) {
+            bindingResult.addError(
+                    new FieldError("changePasswordDto", "newPasswordCf", "The two passwords do not match.")
+            );
+        } else if (!changePasswordDto.oldPassword.equals(user.getPassword())) {
+            bindingResult.addError(
+                    new FieldError("changePasswordDto", "oldPassword", "Invalid password.")
+            );
+        }
+
+        if(bindingResult.hasErrors()) {
+            return "profile_page/t_profile_page";
+        }
+
+        userService.changePassword(user, changePasswordDto.newPassword);
+
+        return "redirect:/profiles/" + user.getId();
     }
 }
--- src/main/java/com/ons/group2/ons_client_project/controllers/UserProfileController.java
+++ src/main/java/com/ons/group2/ons_client_project/controllers/UserProfileController.java
@@ -1,6 +1,7 @@
 package com.ons.group2.ons_client_project.controllers;
 
 import com.ons.group2.ons_client_project.model.User;
+import com.ons.group2.ons_client_project.model.dto.account.ChangePasswordDto;
 import com.ons.group2.ons_client_project.repository.UserRepository;
 import com.ons.group2.ons_client_project.service.UserService;
 import org.springframework.stereotype.Controller;
@@ -8,16 +9,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.util.Optional;
 
 @Controller
 @RequestMapping("/profiles")
+@SessionAttributes({"user"})
 public class UserProfileController {
 
     private final UserService userService;
 
-    public UserProfileController(UserRepository userRepository, UserService userService) {
+    public UserProfileController(UserService userService) {
         this.userService = userService;
     }
 
@@ -30,7 +33,8 @@ public class UserProfileController {
             return "404";
         }
 
-        model.addAttribute("userInfo", user.get());
+        model.addAttribute("changePasswordDto", new ChangePasswordDto());
+        model.addAttribute("user", user.get());
 
         return "profile_page/t_profile_page";
     }
