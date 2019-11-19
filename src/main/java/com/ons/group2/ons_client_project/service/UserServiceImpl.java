8db2894aa72a711d8d62f9ddf23b4990d793c73d
Helen Abraham <AbrahamH1@cardiff.ac.uk>
2019-11-19 13:07:50 +0000

    BCrypyptPasswordEncoder Works!

--- src/main/java/com/ons/group2/ons_client_project/service/UserServiceImpl.java
+++ src/main/java/com/ons/group2/ons_client_project/service/UserServiceImpl.java
@@ -2,15 +2,15 @@ package com.ons.group2.ons_client_project.service;
 
 import com.ons.group2.ons_client_project.model.User;
 import com.ons.group2.ons_client_project.repository.UserRepository;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-//    @Autowired
-//    BCryptPasswordEncoder encoder;
+    @Autowired
+    BCryptPasswordEncoder encoder;
 
     @Autowired
     UserRepository userRepository;
@@ -18,7 +18,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public void saveUser(User user) {
         user.setPassword(user.getPassword());
-//        user.setPassword(encoder.encode(user.getPassword())); //TODO: Spring boot security
+        user.setPassword(encoder.encode(user.getPassword())); //TODO: Spring boot security
         userRepository.save(user);
     }
 
