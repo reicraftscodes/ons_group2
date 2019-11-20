898604af2f1b79d7ea5af56d054662ee3f925b6f
Soma Somorjai <somorjais@cardiff.ac.uk>
2019-11-20 19:35:28 +0000

    Readding cors request disable

--- src/main/java/com/ons/group2/ons_client_project/config/SecurityConfig.java
+++ src/main/java/com/ons/group2/ons_client_project/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .anyRequest().permitAll();
-
+        http.cors().disable();
     }
 
 }
