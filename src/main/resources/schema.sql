d8a74e9259cacee48341da35c8dbc8dbbbca78b4
Soma Somorjai <somorjais@cardiff.ac.uk>
2019-11-20 21:47:25 +0000

    Adding user skills schemas

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,7 @@
 USE ons_db;
 
 DROP TABLE IF EXISTS `user_table`;
+DROP TABLE IF EXISTS `user_skills`;
 
 CREATE TABLE `user_table` (
 	`user_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -10,4 +11,13 @@ CREATE TABLE `user_table` (
     `img_url` varchar(255) DEFAULT NULL,
     PRIMARY KEY(`user_id`),
     KEY `EMAIL_INDEX` (`email`)
+);
+
+CREATE TABLE `user_skills` (
+      `id` int(11) NOT NULL AUTO_INCREMENT,
+      `user_id` int(11) NOT NULL,
+      `title` varchar(255) DEFAULT NULL,
+      `description` varchar(255) DEFAULT NULL,
+      `confidence` tinyint DEFAULT NULL,
+      PRIMARY KEY(`id`)
 );
