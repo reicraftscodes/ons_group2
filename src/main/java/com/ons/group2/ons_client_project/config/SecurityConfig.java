8dceb252ad63a0db583ba36a84efff937da01026
Soma Somorjai <somorjais@cardiff.ac.uk>
2019-11-20 19:29:33 +0000

    Add crsf token to ajax request.

--- src/main/java/com/ons/group2/ons_client_project/config/SecurityConfig.java
+++ src/main/java/com/ons/group2/ons_client_project/config/SecurityConfig.java
@@ -26,7 +26,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().disable();
+        http.authorizeRequests()
+                .anyRequest().permitAll();
+
     }
 
 }
