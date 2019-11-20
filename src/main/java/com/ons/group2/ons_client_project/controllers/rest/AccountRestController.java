f17a996346565f6d4f473ab7095aaa869ad269e6
Soma Somorjai <somorjais@cardiff.ac.uk>
2019-11-20 00:32:45 +0000

    Update gitignore for file upload.

---
+++ src/main/java/com/ons/group2/ons_client_project/controllers/rest/AccountRestController.java
@@ -0,0 +1,4 @@
+package com.ons.group2.ons_client_project.controllers.rest;
+
+public class AccountRestController {
+}
---
+++ src/main/java/com/ons/group2/ons_client_project/utils/StorageException.java
@@ -0,0 +1,4 @@
+package com.ons.group2.ons_client_project.utils;
+
+public class StorageException {
+}
