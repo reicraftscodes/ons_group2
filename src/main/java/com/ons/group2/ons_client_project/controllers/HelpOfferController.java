5dc8456a4e0b0cce28aa4fa2921b49dea62a3d49
V La Roche <larochev111@gmail.com>
2019-11-23 12:54:15 +0000

    - changed template return to include actual path to template

--- src/main/java/com/ons/group2/ons_client_project/controllers/HelpOfferController.java
+++ src/main/java/com/ons/group2/ons_client_project/controllers/HelpOfferController.java
@@ -1,5 +1,6 @@
 package com.ons.group2.ons_client_project.controllers;
 
+import com.ons.group2.ons_client_project.model.dto.help_offer.NewHelpOfferDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +11,8 @@ public class HelpOfferController {
 
     @GetMapping("/createOffer")
     public String createOffer(Model model){
-        return "t_help_offer_form";
+        model.addAttribute("NewHelpOfferDto", new NewHelpOfferDto());
+        return "help_offer_and_help_requests/t_help_offer_form";
     }
 
     @PostMapping("/submitOffer")
