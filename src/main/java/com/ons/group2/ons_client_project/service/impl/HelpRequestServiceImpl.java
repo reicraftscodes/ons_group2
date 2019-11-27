f327e2909a651237e1d2cd90eed2964d4c585e69
V La Roche <larochev111@gmail.com>
2019-11-27 14:15:50 +0000

    - created HelpRequestService interface and implementation

---
+++ src/main/java/com/ons/group2/ons_client_project/service/impl/HelpRequestServiceImpl.java
@@ -0,0 +1,27 @@
+package com.ons.group2.ons_client_project.service.impl;
+
+import com.ons.group2.ons_client_project.model.HelpRequest;
+import com.ons.group2.ons_client_project.service.HelpRequestService;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class HelpRequestServiceImpl implements HelpRequestService {
+
+    private HelpRequestRepository helpRequestRepository;
+
+    public HelpRequestServiceImpl(HelpRequestRepository helpRequestRepository) {
+        this.helpRequestRepository = helpRequestRepository;
+    }
+
+    @Override
+    public HelpRequest save(HelpRequest helpRequest) {
+        return helpRequestRepository.save(helpRequest);
+    }
+
+    @Override
+    public Optional<HelpRequest> findById(Long id) {
+        return helpRequestRepository.findById(id);
+    }
+}
