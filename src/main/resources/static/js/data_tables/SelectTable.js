760fe0805c49a9ece9724de20495b69c1d696b95
V La Roche <larochev111@gmail.com>
2019-12-02 14:21:45 +0000

    All tables are shown and searchable however search bar is showed for all even when the table is hidden

--- src/main/resources/static/js/data_tables/SelectTable.js
+++ src/main/resources/static/js/data_tables/SelectTable.js
@@ -1,7 +1,7 @@
 // used to hide all tables but the one selected by the user
 function hideAllButOne(optionToShow){
-    var options = ["mydatatable","Help Offers"];
-    for (var i = 0; i < 2; i++) {
+    var options = ["mydatatable","Help Offers","Help Requests"]; // Options in the dropdown MAKE SURE TO UPDATE IF ADDING MORE OPTIONS
+    for (var i = 0; i < options.length; i++) {
         var idPrefix = "#";
         var tableName = idPrefix.concat(options[i].replace(/ /g,"_").toLowerCase());
         if(options[i] == optionToShow){
@@ -16,10 +16,13 @@ function hideAllButOne(optionToShow){
 
 
 $(document).ready(function(){
+
+    $('#help_offers').hide();
+    $('#help_requests').hide();
+
     // gets the currently selected table to search
     $("select.selectedSearchCategory").change(function(){
         var selectedCategory = $(this).children("option:selected").val();
-        console.log(selectedCategory);
         hideAllButOne(selectedCategory);
     });
 
