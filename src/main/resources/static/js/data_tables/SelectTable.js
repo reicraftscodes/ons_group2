3f87693c74b58b27464fe3aedcb0589783ff3d19
V La Roche <larochev111@gmail.com>
2019-12-02 13:20:08 +0000

    Table is hidded when not selected

--- src/main/resources/static/js/data_tables/SelectTable.js
+++ src/main/resources/static/js/data_tables/SelectTable.js
@@ -1,9 +1,26 @@
+// used to hide all tables but the one selected by the user
+function hideAllButOne(optionToShow){
+    var options = ["mydatatable","Help Offers"];
+    for (var i = 0; i < 2; i++) {
+        var idPrefix = "#";
+        var tableName = idPrefix.concat(options[i].replace(/ /g,"_").toLowerCase());
+        if(options[i] == optionToShow){
+            // show selected table
+            $(tableName).show();
+            console.log("showing ".concat(tableName))
+        }else{
+            $(tableName).hide();
+        }
+    }
+}
+
+
 $(document).ready(function(){
-
+    // gets the currently selected table to search
     $("select.selectedSearchCategory").change(function(){
-
         var selectedCategory = $(this).children("option:selected").val();
-
+        console.log(selectedCategory);
+        hideAllButOne(selectedCategory);
     });
 
 });
