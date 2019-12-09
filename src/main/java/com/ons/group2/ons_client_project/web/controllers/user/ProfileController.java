7e10c192202c718aeb74b27b87e4c9ca2375f8ed
Soma Somorjai <somorjais@cardiff.ac.uk>
2019-12-09 21:31:36 +0000

    Update logged in user info too upon edit.

--- src/main/java/com/ons/group2/ons_client_project/web/controllers/user/ProfileController.java
+++ src/main/java/com/ons/group2/ons_client_project/web/controllers/user/ProfileController.java
@@ -2,17 +2,21 @@ package com.ons.group2.ons_client_project.web.controllers.user;
 
 import com.ons.group2.ons_client_project.model.dto.account.SafeUserDetails;
 import com.ons.group2.ons_client_project.model.dto.account.UpdateUserInfoDto;
+import com.ons.group2.ons_client_project.security.UserPrincipal;
 import com.ons.group2.ons_client_project.service.UserService;
 import com.ons.group2.ons_client_project.service.UserSkillService;
 import com.ons.group2.ons_client_project.utils.UserUtils;
 import javassist.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
 
@@ -66,7 +70,12 @@ public class ProfileController {
             @Valid UpdateUserInfoDto userInfoDto,
             Model model,
             BindingResult bindingResult,
+            SessionStatus sessionStatus,
             Authentication auth) {
+
+        if(bindingResult.hasErrors()) {
+            return "user/index";
+        }
 
         var user = UserUtils.getUserFromAuth(auth);
 
@@ -74,18 +83,26 @@ public class ProfileController {
         userInfoDto.setUserId(user.getId());
 
         try {
-            userService.updateUser(userInfoDto);
+            user = userService.updateUser(userInfoDto);
+
         } catch (IllegalArgumentException e) {
             log.error(String.format("Error updating user profile: %s", e.getMessage()));
         } catch (NotFoundException e) {
             log.error(e.getMessage());
+        } finally {
+            ((UserPrincipal) auth.getPrincipal()).setUser(user);
+            SecurityContextHolder.getContext().setAuthentication(auth); // refresh logged in user details
+
+            var formPreFilled = new UpdateUserInfoDto(null, user.getFirstName(),
+                    user.getLastName(), user.getEmail());
+
+            model.addAttribute("updateFrom", formPreFilled);
         }
 
-        var formPreFilled = new UpdateUserInfoDto(null, user.getFirstName(), user.getLastName(), user.getEmail());
 
-        model.addAttribute("updateFrom", formPreFilled);
+
         model.addAttribute("content", "profile");
 
-        return "/user/index";
+        return "user/index";
     }
 }
