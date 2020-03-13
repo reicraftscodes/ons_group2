78af33dc1e7bf1ceaf22643514cba61f350ba825
Vincenzo La Roche Hamond <larochehamondv@cardiff.ac.uk>
2020-03-13 13:41:14 +0000

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 use ons_db;
-INSERT INTO user_table (user_id,user_name,email,password,img_url) values (null,"testUser","testuser@gmail.com","testPassword",
+INSERT INTO user (user_id,user_name,email,password,img_url) values (null,"testUser","testuser@gmail.com","testPassword",
 "www.google.com");
 
 
